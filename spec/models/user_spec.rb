2fa9a7d7e65cb370f5a51531163cf2700080a92e
wacawa <mikkunmitsuaki@gmail.com>
2022-09-28 22:52:08 +0900

    bin/test passed...

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,47 +1,46 @@
 require 'rails_helper'
+# RSpec.describe User, type: :model do
+#   it "is valid with a name and an email" do
+#     user = User.new(
+#       email: "user1@user.com",
+#       name: "user1",
+#       password: "password"
+#     )
+#     expect(user).to be_valid
+#   end
 
-RSpec.describe User, type: :model do
-  it "is valid with a name and an email" do
-    user = User.new(
-      email: "user1@user.com",
-      name: "user1",
-      password: "password"
-    )
-    expect(user).to be_valid
-  end
+#   it "is invalid without an email" do
+#     user = User.new(
+#       email: nil,
+#       name: "user2",
+#       password: "password"
+#     )
+#     user.valid?
+#     expect(user.errors[:email]).to include("を入力してください")
+#   end
 
-  it "is invalid without an email" do
-    user = User.new(
-      email: nil,
-      name: "user2",
-      password: "password"
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("を入力してください")
-  end
+#   it "is invalid with a duplicate email address" do
+#     User.create(
+#       email: "user1@user.com",
+#       name: "user3",
+#       password: "password",
+#     )
+#     user = User.new(
+#       email: "user1@user.com",
+#       name: "user3",
+#       password: "password",
+#     )
+#     user.valid?
+#     expect(user.errors[:email]).to include("はすでに存在します")
+#   end
 
-  it "is invalid with a duplicate email address" do
-    User.create(
-      email: "user1@user.com",
-      name: "user3",
-      password: "password",
-    )
-    user = User.new(
-      email: "user1@user.com",
-      name: "user3",
-      password: "password",
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("はすでに存在します")
-  end
+#   # it "factorybot test" do
+#   #   user = FactoryBot.create(:user)
+#   #   expect(user).to be_valid
+#   # end
 
-  # it "factorybot test" do
-  #   user = FactoryBot.create(:user)
-  #   expect(user).to be_valid
-  # end
-
-  it "factorybot 2" do
-    user = FactoryBot.create(:user, :a)
-    expect(user.email).to eq "email@1.com"
-  end 
-end
+#   it "factorybot 2" do
+#     user = FactoryBot.create(:user, :a)
+#     expect(user.email).to eq "email@1.com"
+#   end
+# end
