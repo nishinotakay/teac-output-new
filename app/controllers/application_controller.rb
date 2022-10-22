c2df8cfd7e0034537c9038fd05a156cfa0acc871
untenht <untenht2021@gmail.com>
2022-10-22 10:14:25 +0900

    herokuデプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user
 
   def after_sign_in_path_for(resource)
     case resource
@@ -20,5 +21,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+
+  def set_current_user
+    @current_user = User.find_by(id: session[user_session])
   end
 end
