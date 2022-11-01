4f3d779b5aa3f2b17d4b3569d72cab693e541087
sh19870501 <sh11821783@icloud.com>
2022-11-02 00:59:08 +0900

    add: PostのRspecモデルテスト機能追加及びテスト成功

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,15 @@
 FactoryBot.define do
+  # postに「title」という別名を命名
   factory :post do
-    body { "MyText" }
-    youtube_url { "MyString" }
+    # userモデルアソシエーション
+    association :user
+    # 40文字のランダム文字列
+    # (バリテーションエラーのテストの為、モデルより1文字多く記載）
+    title { Faker::Lorem.characters(number:31) }
+    # 250文字のランダム文字列
+    # (バリテーションエラーのテストの為、モデルより1文字多く記載）
+    body { Faker::Lorem.characters(number:241) }
+    # 400文字のランダム文字列
+    youtube_url { Faker::Lorem.characters(number:400) }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  # aliases: ⇨ Postのモデルテストで起きた『KeyError: Factory not registered:~』に対処 
   factory :user do
     trait :a do
       id { 1 }
