7bb8d401a9c22b3063b88ab82a3dca06ab1ee305
sh11821783 <57516674+sh11821783@users.noreply.github.com>
2022-11-02 01:35:25 +0900

    Update posts.rb

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
   factory :post do
     # userモデルアソシエーション
     association :user
-    # 40文字のランダム文字列
+    # 31文字のランダム文字列
     # (バリテーションエラーのテストの為、モデルより1文字多く記載）
     title { Faker::Lorem.characters(number:31) }
-    # 250文字のランダム文字列
+    # 241文字のランダム文字列
     # (バリテーションエラーのテストの為、モデルより1文字多く記載）
     body { Faker::Lorem.characters(number:241) }
     # 400文字のランダム文字列
