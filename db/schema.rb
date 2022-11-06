926e6ec42f728f8e866752e3d6d6c298783fdb33
manabutice <manabutice@gmail.com>
2022-11-06 15:48:58 +0900

    マイグレート取消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_23_150420) do
+ActiveRecord::Schema.define(version: 2022_07_16_234556) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -105,16 +105,6 @@ ActiveRecord::Schema.define(version: 2022_09_23_150420) do
     t.index ["unlock_token"], name: "index_managers_on_unlock_token", unique: true
   end
 
-  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.string "youtube_url"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
   create_table "profiles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
@@ -160,6 +150,5 @@ ActiveRecord::Schema.define(version: 2022_09_23_150420) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "articles", "users"
-  add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
 end
