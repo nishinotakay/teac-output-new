e1870b6d5b2314f517a9ebad60df746fc77f16b6
wacawa <mikkunmitsuaki@gmail.com>
2022-11-12 18:27:59 +0900

    遷移関連のsystem spec途中。

--- spec/system/articles_spec.rb
+++ spec/system/articles_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Articles', type: :system do
-  let(:user) { create(:user, :a, confirmed_at: Date.today) }
+  let(:user_a) { create(:user, :a, confirmed_at: Date.today) }
+  let(:user_b) { create(:user, :b, confirmed_at: Date.today) }
+  let(:article) { create(:article, user: user_a) }
 
   describe 'redirect to #X' do
-    let(:article) { create(:article, user: user) }
-    before { sign_in(user) }
+    before { sign_in(user_a); article }
 
     context 'X = index' do
       it 'success' do
-        article
         visit users_articles_path
         expect(current_path).to eq users_articles_path
-        page.save_screenshot 'index.png'
         expect(page).to have_content '記事一覧'
         expect(page).to have_content 'タイトル〜サブタイトル〜'
         expect(page).to have_content '投稿者'
@@ -20,26 +19,67 @@ RSpec.describe 'Articles', type: :system do
         expect(page).to have_content article.title
         expect(page).to have_content article.sub_title
         expect(page).to have_content article.user.name
+        expect(page).to have_content article.created_at.strftime('%-m/%d %-H:%M')
       end
     end
 
     context 'X = dashboards' do
       it 'success' do
-        click_link 'DashBoard'
-        click_link '記事作成'
-        # click_link '記事投稿' リネームしたプルリクのマージ待ち
-        # expect(page).to have_content('記事投稿', count: 2)
-        expect(page).to have_content('記事投稿', count: 1)
+        visit users_dash_boards_path
+        expect(current_path).to eq users_dash_boards_path
+        expect(page).to have_content '投稿した記事一覧'
+        expect(page).to have_content 'タイトル〜サブタイトル〜'
+        expect(page).to have_content '投稿日'
+        expect(page).to_not have_content '投稿者'
+        expect(page).to have_content article.title
+        expect(page).to have_content article.sub_title
+        expect(page).to have_content article.created_at
+        expect(page).to have_content article.created_at.strftime('%-m/%d %-H:%M')
       end
     end
 
     context 'X = show' do
-      it 'success' do
+      context 'writer' do
+        it 'success' do
+          visit users_article_path(article)
+          expect(current_path).to eq users_article_path(article)
+          expect(page).to have_content '編集'
+          expect(page).to have_content '削除'
+          expect(page).to have_content article.title
+          expect(page).to have_content article.sub_title
+          expect(page).to have_content article.content
+          expect(page).to_not have_content article.user.name, count: 2
+        end
+      end
+
+      context 'non_writer' do
+        it 'success' do
+          click_link 'ログアウト'
+          sign_in(user_b)
+          visit users_article_path(article)
+          expect(current_path).to eq users_article_path(article)
+          expect(page).to_not have_content '編集'
+          expect(page).to_not have_content '削除'
+          expect(page).to have_content '投稿者'
+          expect(page).to have_content article.user.name
+          expect(page).to have_content article.title
+          expect(page).to have_content article.sub_title
+          expect(page).to have_content article.content
+          page.save_screenshot 'index.png'
+        end
       end
     end
 
+
     context 'X = new' do
       it 'success' do
+        visit new_users_article_path
+        expect(current_path).to eq new_users_article_path
+        expect(page).to have_content "記事投稿"
+        expect(page).to have_content article.title
+        expect(page).to have_content article.sub_title
+        expect(page).to have_content article.content
+        expect(page).to_not have_content article.user.name, count: 2
       end
     end
 
