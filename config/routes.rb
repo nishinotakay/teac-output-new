bd8a2afad7f6b4c979519fa829834521f9aa1c7e
nishinotakaya <takaya314boxing@gmail.com>
2022-12-02 21:07:37 +0900

    fix/ルート解消

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,6 @@ Rails.application.routes.draw do
     resources :users, only: [:index]
     end
     resources :profiles
-    
   end
   # =================================================================
 
