d996721b027c15347ac49f834a93237dcb03cf41
suganmo <suganmo84@gmail.com>
2022-12-11 15:49:24 +0900

    bug/admin/after_sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,6 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
-  # ログイン済ユーザーのみにアクセスを許可する
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_current_user
-
-  # protect_from_forgery
-  def after_sign_in_path_for(resource)#usersコントローラーのshowアクションを呼び出すパスを設定  ここに追加するのか？
-    users_show_path
-  end
-
-  before_action :set_current_user
 
   def after_sign_in_path_for(resource)
     case resource
@@ -30,9 +18,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
-  end
-
-  def set_current_user
-    @current_user = User.find_by(id: session[user_session])
   end
 end
