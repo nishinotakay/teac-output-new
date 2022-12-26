928f9005f23bbf39f8b0a7d023a8fc48f731bcd1
mockysunshinework <mockysunshinework@gmail.com>
2022-12-26 11:44:07 +0900

    fix_comment_out, make_new_line

--- app/controllers/users/tweets_controller.rb
+++ app/controllers/users/tweets_controller.rb
@@ -1,10 +1,3 @@
-# class Users::TweetsController < ApplicationController
-#   def index
-#     binding.pry
-#     @tweets = Tweet.all
-#   end
-# end
-
 module Users
   class TweetsController < Users::Base
     def index
