63866508d5a83dd219bbaa0f5091587b5e5cef12
mockysunshinework <mockysunshinework@gmail.com>
2022-12-29 18:47:20 +0900

    add_new_tweet

--- app/controllers/users/tweets_controller.rb
+++ app/controllers/users/tweets_controller.rb
@@ -4,5 +4,25 @@ module Users
       @users = User.all
       @tweets = Tweet.all
     end
+
+    def new
+      @tweet = current_user.tweets.new
+    end
+
+    def create
+      @tweet =current_user.tweets.new(tweet_params)
+      if @tweet.save
+        flash[:success] = "つぶやきを作成しました。"
+        redirect_to users_tweets_url(current_user)
+      else
+        render :new
+      end 
+    end
+
+    private
+      
+      def tweet_params
+        params.require(:tweet).permit(:post)
+      end
   end
 end
