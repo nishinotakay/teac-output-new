9d11159043a27c80779cf637965842b375d78d20
satoshir <satoshi.ikezawa3@gmail.com>
2023-01-10 21:33:56 +0900

    修正

--- app/controllers/users/dash_boards_controller.rb
+++ app/controllers/users/dash_boards_controller.rb
@@ -2,6 +2,7 @@ module Users
   class DashBoardsController < Users::Base
     def index
       @articles = current_user.articles.order(updated_at: 'DESC')
+      @profiles = Profile.all
     end
   end
 end
