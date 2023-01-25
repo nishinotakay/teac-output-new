4f6da7fb6beb046b4f739735cec317df2cd32389
satoshir <satoshi.ikezawa3@gmail.com>
2023-01-25 22:15:09 +0900

    修正

---
+++ app/controllers/users/inquiries_controller.rb
@@ -0,0 +1,34 @@
+module Users
+  class InquiriesController < Users::Base
+    def index
+     @inquiry = Inquiry.all
+    end
+
+    def show
+    end
+
+    def new
+      @inquiry = current_user.inquiries.new
+    end
+
+    def create
+      @inquiry = current_user.inquiries.new(inquiry_params)
+      if @inquiry.save
+         redirect_to new_users_inquiry_path , flash: {success: "問い合わせを投稿しました。"}
+      else
+         flash.now[:danger] = '問い合わせ投稿出来ませんでした。'
+         render :new
+      end
+    end
+
+    def edit
+    end
+  
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:subject, :content)
+  end
+
+  end
+end
