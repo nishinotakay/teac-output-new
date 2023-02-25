46bec0e85a9e1bc45252dc6265721a1dba6cd518
wacawa <mikkunmitsuaki@gmail.com>
2023-02-25 18:43:10 +0900

    finish admin_article_index_filter

--- app/controllers/admins/dash_boards_controller.rb
+++ app/controllers/admins/dash_boards_controller.rb
@@ -1,7 +1,15 @@
 module Admins
   class DashBoardsController < Admins::Base
     def index
-      @articles = Article.all.order(updated_at: 'DESC').page(params[:page]).per(30)
+      params[:order] ||= 'DESC'
+      filter = {author: params[:author], title: params[:title], subtitle: params[:subtitle],
+        content: params[:content], start: params[:start], finish: params[:finish]}
+      if @paginate = filter.compact.blank?
+        @articles = Article.order(created_at: params[:order]).page(params[:page]).per(30) 
+      else
+        filter[:order] = params[:order]
+        @articles = Article.sort_filter(filter)
+      end
     end
   end
 end
