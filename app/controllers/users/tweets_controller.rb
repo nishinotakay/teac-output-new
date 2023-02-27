6181fe1df54d8649db3a4eea281fd7c4a0f23bc7
mockysunshinework <mockysunshinework@gmail.com>
2023-02-27 11:43:38 +0900

    fix/add_before_action_to_tweets_controller

--- app/controllers/users/tweets_controller.rb
+++ app/controllers/users/tweets_controller.rb
@@ -2,6 +2,9 @@ module Users
   class TweetsController < Users::Base
     # Userがログインしていないと、投稿を作成・編集・削除できない
     before_action :authenticate_user!, only: [:show, :index, :new, :create, :edit, :update, :destroy, :index_user]
+    before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+    # 投稿をしたユーザー以外は編集・削除できない
+    before_action :correct_tweet_user, only: [:edit, :update, :destroy]
 
     def index
       @users = User.all
@@ -9,7 +12,6 @@ module Users
     end
 
     def show
-      @tweet = Tweet.find(params[:id])
       @comments = @tweet.comments.all
       @comment = current_user.comments.new
     end
@@ -25,11 +27,9 @@ module Users
     end
 
     def edit
-      @tweet = Tweet.find(params[:id])
     end
 
     def update
-      @tweet = Tweet.find(params[:id])
       if @tweet.update(tweet_params)
         @tweet.save
         flash[:success] = "編集成功しました。"
@@ -40,7 +40,6 @@ module Users
     end
 
     def destroy
-      @tweet = Tweet.find(params[:id])
       if @tweet.destroy
         flash[:success] = "削除に成功しました。"
         redirect_to users_tweets_url
@@ -56,6 +55,17 @@ module Users
       
     def tweet_params
       params.require(:tweet).permit(:post)
+    end
+
+    # beforeフィルター
+
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+
+    def correct_tweet_user
+      @tweet = Tweet.find(params[:id])
+      redirect_to root_url if @tweet.user != current_user
     end
   end
 end
