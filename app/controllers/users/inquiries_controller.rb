9404052dc105f4e4df1b23c4831b8004f9438755
satoshir <satoshi.ikezawa3@gmail.com>
2023-04-14 19:35:18 +0900

    検索

--- app/controllers/users/inquiries_controller.rb
+++ app/controllers/users/inquiries_controller.rb
@@ -1,10 +1,24 @@
 module Users
   class InquiriesController < Users::Base
     def index
-      @inquiry = Inquiry.all
+      @inquiry = Inquiry.where(hidden: false)
+    end
+
+    def hide_index
+      @inquiry = Inquiry.where(hidden: true)
     end
 
     def show
+      @inquiry = Inquiry.where(id: params[:id])
+    end
+
+    def update
+      if @inquiry.update(inquiry_params)
+        redirect_to users_inquiry_path, flash: {success: "問い合わせを投稿しました。"}
+      else
+        flash.now[:danger] = '問い合わせ投稿出来ませんでした。'
+        redirect_to users_inquiry_path
+      end
     end
 
     def new
@@ -20,8 +34,19 @@ module Users
         render :new
       end
     end
+
+    def update
+      @inquiry = Inquiry.find(params[:id])
+      if @inquiry.update(hidden: true)
+        flash[:notice] = 'お問い合わせを非表示にしました'
+        redirect_to users_inquiries_path
+      else
+        flash[:notice] = 'お問い合わせを表示しました'
+      end
+    end
   
     private
+
 
     def inquiry_params
       params.require(:inquiry).permit(:subject, :content)
