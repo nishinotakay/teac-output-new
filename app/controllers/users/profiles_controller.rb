c720ed261fd571f78efa1ba020b0c353cb3109dd
nagaminejun <nagaminejun1988@gmail.com>
2023-04-16 21:16:10 +0900

    プロフィールで名前編集できるようにした

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -30,8 +30,9 @@ module Users
 
     def create
       @profile = current_user.build_profile(profile_params)
-      @profile.name = current_user.name
+      #@profile.name = current_user.name
       if @profile.save
+        @profile.user.update(profile_params[:user_attributes])
         redirect_to users_profiles_path, notice: 'プロフィール情報の入力が完了しました'        
       else
         render :new
@@ -79,7 +80,7 @@ module Users
 
     def profile_params
       params.require(:profile).permit(
-        :purpose, :image, :created_at, :learning_start, :birthday, :gender
+        :purpose, :image, :created_at, :learning_start, :birthday, :gender, user_attributes: [:name]
       ).merge(user_id: current_user.id)
     end
   end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,7 @@
 class Profile < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  accepts_nested_attributes_for :user
   
   validates :learning_start, presence: true
   validates :purpose, presence: true
