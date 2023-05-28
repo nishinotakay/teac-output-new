cbb34474a864dffc012424037301382daa1e5e10
akito sato <eight2019@outlook.jp>
2023-05-29 04:40:29 +0900

    記事一覧絞り込み検索修正・動画投稿編集、削除修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,10 +13,11 @@ class Article < ApplicationRecord
   validates :content, presence: true
 
   def self.sort_filter(filter)
-    start = filter[:start].blank? ? '2022-01-01' : filter[:start]
-    finish = filter[:finish].blank? ? Date.current : filter[:finish]
-    start = start.to_datetime.since(9.hours).beginning_of_day
-    finish = finish.to_datetime.since(9.hours).end_of_day
+    start = filter[:start].blank? ? Time.zone.parse('2022-01-01') : filter[:start].to_datetime
+    finish = filter[:finish].blank? ? Time.zone.now.to_date : filter[:finish].to_datetime
+  
+    start = start.since(9.hours).beginning_of_day
+    finish = finish.since(9.hours).end_of_day
     articles = order("articles.created_at #{filter[:order]}")
       .where(['title like ? and sub_title like ? and content like ?',
               "%#{filter[:title]}%", "%#{filter[:subtitle]}%", "%#{filter[:content]}%"])
