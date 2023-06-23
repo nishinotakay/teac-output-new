4d249a56aa6c1991a8130f2e3ebdffbe4ee58c38
satoshir <satoshi.ikezawa3@gmail.com>
2023-06-24 01:18:30 +0900

    検索機能index修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -9,22 +9,18 @@ class Profile < ApplicationRecord
 
   def self.sort_filter(order, filter)
     profiles = self.all
-    # Apply filters
     filter.each do |ord, fit|
-      # Check if it's a text field for fuzzy search
       if [:name, :purpose].include?(ord) && fit.present?
         profiles = profiles.where("lower(#{ord}) LIKE ?", "%#{fit.downcase}%")
-      # For non-text fields use exact match
+
       elsif fit.present?
         profiles = profiles.where(ord => fit)
       end
     end
-    # Apply sorting
     order.each do |ord, direction|
       profiles = profiles.order(ord => direction) if direction.present?
     end
     return profiles
   end
-
 end
 
