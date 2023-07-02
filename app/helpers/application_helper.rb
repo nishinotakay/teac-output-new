7d15edfb12b6b9149009a4c890f0fb50443eee82
mockysunshinework <mockysunshinework@gmail.com>
2023-07-02 16:42:34 +0900

    Revert "feat/format_and_linkify_text_method"
    This reverts commit a3979900326841f3cc95dc7a969588ca9962aae9, reversing
    changes made to 404767a7fdf3df6729959954f46cced4b3718489.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
   def page_body_id(params)
     "#{params[:controller].gsub(/\//, '-')}-#{params[:action]}"
   end
-
   # 危険性があるhtmlタグを除去、改行を<br>に置換、空白を半角スペースに置換、URLをリンク化。
   def format_and_linkify_text(str)
     sanitized_str = sanitize(str)
