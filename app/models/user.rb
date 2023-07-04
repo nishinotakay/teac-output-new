fffa4875763fe5d3fc100af63458b48959bc48dc
mockysunshinework <mockysunshinework@gmail.com>
2023-07-04 14:34:05 +0900

    fix/user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :tweets, dependent: :destroy
   has_many :inquiries, dependent: :destroy
-  has_many :comments
   has_many :comments, dependent: :destroy
   has_many :article_comments, dependent: :destroy
 
