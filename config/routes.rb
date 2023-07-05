9dfea6844ceb47de768efd028631c71e6e279539
orikotanaka <oriko644@gmail.com>
2023-07-05 15:46:22 +0900

    テナント画面ヘッダー・サイドバーレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -23,19 +23,9 @@ Rails.application.routes.draw do
       end
     end
     resources :posts do
-      collection do # idを外す
-        # 全ユーザー投稿一覧（/admins/posts/index_1）
-        get 'index_1'
-      end
-      # 全ユーザー詳細ページ（/admins/posts/:id/show_1）
       member do # id付与
-        get 'show_1'
         get 'show'
         delete 'show'
-        # 全ユーザー編集ページ（/admins/posts/:id/edit_1）
-        get 'edit_1'
-        # 全ユーザー編集ページの更新（/admins/posts/:id/update_1(）
-        patch 'update_1'
       end
     end
     namespace :articles do
@@ -68,22 +58,14 @@ Rails.application.routes.draw do
 
   namespace :users do
     resources :dash_boards, only: [:index]
-    resources :articles # , only: %i[index show]
+    resources :articles do
+      resources :article_comments, only: %i[create destroy update] # 記事コメント機能
+    end
     resources :posts
     resources :users, only: [:show]
     resources :posts do
-      collection do # idを外す
-        # 全ユーザー投稿一覧（/users/posts/index_1）
-        get 'index_1'
-      end
-      # 全ユーザー詳細ページ（/users/posts/:id/show_1）
       member do # id付与
-        get 'show_1'
         get 'show'
-        # 全ユーザー編集ページ（/users/posts/:id/edit_1）
-        get 'edit_1'
-        # 全ユーザー編集ページの更新（/users/posts/:id/update_1(）
-        patch 'update_1'
       end
     end
     resources :articles
@@ -118,7 +100,7 @@ Rails.application.routes.draw do
   # =================================================================
 
   # tenant関連========================================================
-  resources :tenants, only: [:index]
+  resources :tenants, only: [:index, :new, :create, :destroy]
 
   # =================================================================
 
