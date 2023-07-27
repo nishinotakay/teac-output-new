7104c50d0eb2efe8542beb29512c24adce84ab9f
nagaminejun <nagaminejun1988@gmail.com>
2023-07-27 20:19:50 +0900

    aritcleのFactoryBot、本文内に画像文字列を追加

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :article do
     sequence(:title) { |n| "rspecテスト、タイトル_#{n}" }
     sequence(:sub_title) { |n| "rspecテスト、サブタイトル_#{n}" }
-    sequence(:content) { |n| "rspecテスト、本文_#{n}" }
+    sequence(:content) { |n| "rspecテスト、本文_#{n} app/assets/images/user_default.png" }
   end
 end
