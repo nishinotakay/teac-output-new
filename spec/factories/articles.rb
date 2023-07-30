80af671df3148b0f33a9df89739bee00ea2859ca
nagaminejun <nagaminejun1988@gmail.com>
2023-07-30 09:53:25 +0900

    factories/articles.rb の sequence(:content)を画像無しへ修正

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :article do
     sequence(:title) { |n| "rspecテスト、タイトル_#{n}" }
     sequence(:sub_title) { |n| "rspecテスト、サブタイトル_#{n}" }
-    sequence(:content) { |n| "rspecテスト、本文_#{n} app/assets/images/user_default.png" }
+    sequence(:content) { |n| "rspecテスト、本文_#{n}" }
+    # sequence(:content) { |n| "rspecテスト、本文_#{n} <img alt=\"画像\" src=\"#{Rails.root.join('spec/fixtures/files/ruby.png')}\">\r\n" }
   end
 end
