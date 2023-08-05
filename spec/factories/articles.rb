bd4651b239b66e9af22175d4126982cf1d38e77e
nagaminejun <nagaminejun1988@gmail.com>
2023-08-05 17:35:47 +0900

    factories/articles.rb のコメントアウト削除

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,9 +2,8 @@
 
 FactoryBot.define do
   factory :article do
-    sequence(:title) { |n| "rspecテスト、タイトル_#{n}" }
-    sequence(:sub_title) { |n| "rspecテスト、サブタイトル_#{n}" }
-    sequence(:content) { |n| "rspecテスト、本文_#{n}" }
-    # sequence(:content) { |n| "rspecテスト、本文_#{n} <img alt=\"画像\" src=\"#{Rails.root.join('spec/fixtures/files/ruby.png')}\">\r\n" }
+    sequence(:title) { |n| "タイトル_#{n}" }
+    sequence(:sub_title) { |n| "サブタイトル_#{n}" }
+    sequence(:content) { |n| "本文_#{n}" }
   end
 end
