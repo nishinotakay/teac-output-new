e802153b769ccc7425e7870a1e4c71b01a59812f
nagaminejun <nagaminejun1988@gmail.com>
2023-10-15 09:41:27 +0900

    profile_imageメソッドif文にpresent?追加

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,6 +2,6 @@ class UserDecorator < ApplicationDecorator
   delegate_all
 
   def profile_image
-    object.profile&.image ? object.profile.image : "user_default.png"
+    object.profile&.image.present? ? object.profile.image : "user_default.png"
   end
 end
