ef98ab703968e7a4d7b3f39033de764b4632fc2b
calmdown1987 <calmdown1987@icloud.com>
2023-10-23 16:45:58 +0900

    users_controllerのアクション名を修正

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,26 +1,28 @@
 module Admins
   class UsersController < Admins::Base
     require 'date'
-    before_action :authenticate_admin!, only: %i[new create edit update destroy users_show user_edit user_destroy admins_show]
+    before_action :authenticate_admin!, only: %i[new create edit update destroy admins_show]
     before_action :find_profile, only: %i[edit update destroy]
 
-    def users_show
-      @user = User.find(params[:format])
+    def show
+      @user = User.find(params[:id])
       @profile = @user.profile
       today = Date.today.strftime('%Y%m%d').to_i
+
       learning_startday = @profile.learning_start.strftime('%Y%m%d').to_i if @profile.present? && @profile.learning_start?
       @study_period = (today - learning_startday) / 10000 if learning_startday.present?
+
       birthday = @profile.birthday.strftime('%Y%m%d').to_i if @profile.present? && @profile.birthday?
       @age = (today - birthday) / 10000 if birthday.present?
     end
 
-    def users_edit
-      @user = User.find(params[:format])
+    def edit
+      @user = User.find(params[:id])
     end
 
-    def user_destroy
-      # データの削除
-      @user = User.find(params[:format])
+    def destroy
+      @user = User.find(params[:id])
+
       if @user.destroy
         flash[:success] = "#{@user.name}のデータを削除しました。"
         # ユーザー 一覧ページへリダイレクト
@@ -31,29 +33,39 @@ module Admins
     end
 
     def index
-      order = { id: params[:ord_id], name: params[:ord_name], email: params[:ord_email], articles: params[:ord_articles],
-posts: params[:ord_posts] }.compact
+      order = { 
+        id: params[:ord_id], 
+        name: params[:ord_name], 
+        email: params[:ord_email], 
+        articles: params[:ord_articles],
+        posts: params[:ord_posts] 
+      }.compact
+
       filter = {
-        name: params[:flt_name], email: params[:flt_email],
-        articles_min: params[:flt_articles_min], articles_max: params[:flt_articles_max],
-        posts_min: params[:flt_posts_min], posts_max: params[:flt_posts_max]
+        name: params[:flt_name], 
+        email: params[:flt_email],
+        articles_min: params[:flt_articles_min], 
+        articles_max: params[:flt_articles_max],
+        posts_min: params[:flt_posts_min], 
+        posts_max: params[:flt_posts_max]
       }
+
       @users = if order.count == 1
                  User.sort_filter(order.first,
                    filter)&.page(params[:page])&.per(30)
                else
                  User.all&.page(params[:page])&.per(30)
                end
+
       @profiles = Profile.all
     end
 
-    def admins_show; end
+    def admins_show
+    end
 
     def new
       @profile = Profile.new
     end
-
-    def edit; end
 
     def create
       @profile = Profile.new(profile_params)
@@ -73,21 +85,11 @@ posts: params[:ord_posts] }.compact
       end
     end
 
-    def destroy
-      # データの削除
-      if @profile.destroy
-        flash[:success] = "#{@profile.name}のデータを削除しました。"
-        # 一覧ページへリダイレクト
-        redirect_to admins_users_path
-      else
-        render :index
-      end
-    end
-
     private
 
     def find_profile
-      @profile = Profile.find(params[:id])
+      @user = User.find(params[:id])
+      @profile = @user.profile
     end
 
     def profile_params
