f669434bc4552e59870db9949fd774e9536e77f2
nagaminejun <nagaminejun1988@gmail.com>
2023-11-08 13:14:44 +0900

    コミット履歴を整理するため、git reset --soft実行 記事一覧・投稿した記事一覧画面のテスト実装 未完成のため、コメントアウト残します。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,21 +7,21 @@ FactoryBot.define do
     password { 'password' }
 
     trait :a do
-      id { 1 }
+      id { 2 }
       email { 'email@1.com' }
       name { 'name1' }
       password { 'password1' }
     end
 
     trait :b do
-      id { 2 }
+      id { 3 }
       email { 'email@2.com' }
       name { 'name2' }
       password { 'password2' }
     end
 
     trait :c do
-      id { 3 }
+      id { 4 }
       email { 'email@3.com' }
       name { 'name3' }
       password { 'password3' }
