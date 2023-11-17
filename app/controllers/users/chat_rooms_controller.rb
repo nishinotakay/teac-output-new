8ec829f00a2ab553c9211aafafb48545caf66e41
nagaminejun <nagaminejun1988@gmail.com>
2023-11-17 13:05:28 +0900

    app/controllers/users/chat_rooms_controller.rb　修正
    show アクションに、chatroomのuser_idとcurrent_user.id違えばリダイレクトさせる処理追加

--- app/controllers/users/chat_rooms_controller.rb
+++ app/controllers/users/chat_rooms_controller.rb
@@ -4,7 +4,6 @@ module Users
 
     def create
 
-      # ==========ここから追加==========
       current_user_chat_rooms = ChatRoomUser.where(user_id: current_user.id).map(&:chat_room)
       chat_room = ChatRoomUser.where(chat_room: current_user_chat_rooms, user_id: params[:user_id]).map(&:chat_room).first
       if chat_room.blank?
@@ -13,12 +12,15 @@ module Users
         ChatRoomUser.create(chat_room: chat_room, user_id: params[:user_id])
       end
       redirect_to action: :show, id: chat_room.id
-      # ==========ここまで追加==========
 
     end
 
     def show
       @chat_room = ChatRoom.find(params[:id])
+      unless @chat_room.users.include?(current_user)
+        redirect_to users_profiles_path, notice: "アクセス権限がありません"
+      end
+      # @chat_room_user は チャットの相手 @chat_room_partner が良いか悩む
       @chat_room_user = @chat_room.chat_room_users.where.not(user_id: current_user.id).first.user
       @chat_messages = ChatMessage.where(chat_room: @chat_room)
     end
