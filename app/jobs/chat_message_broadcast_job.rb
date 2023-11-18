901098e077521bcdeec72cb8f4fe6d9f534642c9
nagaminejun <nagaminejun1988@gmail.com>
2023-11-18 21:18:30 +0900

    app/jobs/chat_message_broadcast_job.rb
    相手からチャット送信来た際に、アイコンがブロードキャストに含まれていなかったので、追加実装

--- app/jobs/chat_message_broadcast_job.rb
+++ app/jobs/chat_message_broadcast_job.rb
@@ -1,17 +1,25 @@
 class ChatMessageBroadcastJob < ApplicationJob
   queue_as :default
 
-  # def perform(*args)
-    # Do something later
-  # end
-
   def perform(chat_message)
-    ActionCable.server.broadcast 'chat_room_channel', { chat_message: render_chat_message(chat_message) }
+    chat_room_user = find_chat_room_user(chat_message.chat_room, chat_message.user)
+    ActionCable.server.broadcast 'chat_room_channel', { chat_message: render_chat_message(chat_message, chat_room_user) } # この処理で サーバーのchat_room_channel に chat_message がブロードキャストされ、相手側で高速表示される。
   end
 
   private
 
-    def render_chat_message(chat_message)
-      ApplicationController.renderer.render(partial: 'users/chat_messages/chat_message', locals: { chat_message: chat_message, current_user: chat_message.user })
+    def render_chat_message(chat_message, chat_room_user)
+      ApplicationController.renderer.render(
+        partial: 'users/chat_messages/chat_message',
+        locals: {
+          chat_message: chat_message,
+          current_user: chat_message.user,
+          chat_room_user: chat_room_user
+        }
+      )
+    end
+
+    def find_chat_room_user(chat_room, current_user)
+      chat_room.chat_room_users.where.not(user_id: current_user.id).first.user
     end
 end
