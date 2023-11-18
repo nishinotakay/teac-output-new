10666dd9da4ca54cc9eafd1dee47a4025f2c1ee5
nagaminejun <nagaminejun1988@gmail.com>
2023-11-18 12:34:17 +0900

    コミットもれ

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -1,17 +1,13 @@
 import consumer from "./consumer"
 
-// この行を編集する
-
-
-// ==========ここから追加する==========
-if(/chat_rooms/.test(location.pathname)) {
+if(/chat_rooms/.test(location.pathname)) { // このif分岐内でappChatRoomを定義しないと、全てのページでWebSocket通信が実行されてしまう。linkに/chat_rooms/を含む場合、ということ。
   const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
     connected() {
-      // Called when the subscription is ready for use on the server
+      // これはWebSocketの通信の状態を表すオブジェクト属性の一つ。サーバー上でサブスクリプションを使用する準備ができたら呼び出される。使用用途の詳細は Railsガイド7.1 みて。今後使えそう
     },
   
     disconnected() {
-      // Called when the subscription has been terminated by the server
+      // サブスクリプションがサーバーによって終了されたときに呼び出されます
     },
   
     received(data) {
@@ -19,30 +15,24 @@ if(/chat_rooms/.test(location.pathname)) {
       chatMessages.insertAdjacentHTML('beforeend', data['render_chat_message'] || data['chat_message']);
     },
   
-    // ==========ここから編集する==========
-    // speak: function(chat_message) {
-      //return this.perform('speak', { chat_message: chat_message });
-    //}
-    speak: function(chat_message, chat_room_id) {
+    speak: function(chat_message, chat_room_id) { // ここでspeakというカスタム関数を定義
       return this.perform('speak', { chat_message: chat_message, chat_room_id: chat_room_id });
-    }
-    // ==========ここまで編集する==========
+    } // 引数の'speak'アクションはapp/channels/chat_room_channel.rbへ
   });
   
   $(document).on("keydown", ".chat-room-message-form-textarea", function(e) {
     if (e.keyCode === 13 && !e.shiftKey) { // Shiftキーが押されていないことを確認
-      e.preventDefault(); // フォーム送信を阻止
+      e.preventDefault(); // デフォルトのフォーム送信を阻止する実装
 
-      const messageText = e.target.value.trim();
-      if (messageText === '') {
+      const messageText = e.target.value;
+      if (messageText.match(/^\s*$/)) {
         // 空白でエンターが押された場合は改行を行う
-        e.target.value = e.target.value + "\n";
+        e.target.value += "\n";
       } else {
         // テキストが入力されている場合はメッセージを送信
         const chat_room_id = $(this).data('chat_room_id');
         appChatRoom.speak(messageText, chat_room_id);
         e.target.value = ''; // テキストエリアをクリア
-        e.preventDefault(); // フォーム送信を阻止
       }
     }
   });
