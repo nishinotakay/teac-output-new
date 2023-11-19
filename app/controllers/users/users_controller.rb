32ebe7c7b6480cab9be68770361db5569b910133
nagaminejun <nagaminejun1988@gmail.com>
2023-11-19 23:07:12 +0900

    サイドバーにボタン、ユーザー一覧画面（仮）、ルーティングを作成、

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -2,6 +2,9 @@ module Users
   class UsersController < Users::Base
     before_action :authenticate_user! # 認証ユーザーのみアクセス ログインしていないユーザーを強制的にログインページへ飛ばす
 
+    def index
+    end
+
     def show
       @user = User.find(params[:id]) # 該当idのユーザーレコードをとってくる様に設定
       @profile = current_user.profile
