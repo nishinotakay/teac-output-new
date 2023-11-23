d78dd204df1560f0d1466a233d056d3d9056dc2b
nagaminejun <nagaminejun1988@gmail.com>
2023-11-24 01:26:18 +0900

    不必要なコメントアウトを削除

--- db/migrate/20231110093206_create_chat_room_users.rb
+++ db/migrate/20231110093206_create_chat_room_users.rb
@@ -1,10 +1,8 @@
 class CreateChatRoomUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :chat_room_users do |t|
-      # ==========ここから追加する==========
       t.references :chat_room, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      # ==========ここまで追加する==========
       t.timestamps
     end
   end
