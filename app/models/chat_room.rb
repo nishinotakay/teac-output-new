c84209689d8664fd4070700c3b30df771fb6b9d3
nagaminejun <nagaminejun1988@gmail.com>
2023-12-12 18:04:56 +0900

    chatroom新規作成時に、userが3人以上紐づくことを防ぐ制約実装

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,4 +2,12 @@ class ChatRoom < ApplicationRecord
   has_many :chat_room_users
   has_many :chat_messages
   has_many :users, through: :chat_room_users
+
+  validate :users_count_within_limit, on: :create
+
+  private
+
+  def users_count_within_limit
+    errors.add(:users, 'は最大2人までです。') if users.size >= 2
+  end
 end
