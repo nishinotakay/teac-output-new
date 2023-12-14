5b6debe4ad099725001ba2023b3841ac2c040c31
orikotanaka <oriko644@gmail.com>
2023-12-14 23:50:04 +0900

    rubocop実行

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -13,8 +13,7 @@ class PostDecorator < Draper::Decorator
 
   def formatted_comment_count
     if post_comments.present?
-      h.content_tag(:i, '', class: 'fa-regular fa-comment fa-sm ms-auto') + ' ' + post_comments.count.to_s  
+      h.content_tag(:i, '', class: 'fa-regular fa-comment fa-sm ms-auto') + ' ' + post_comments.count.to_s
     end
   end
-
 end
