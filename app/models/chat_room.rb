e3867e16d5478515fd088d363d18043b9b2e0334
nagaminejun <nagaminejun1988@gmail.com>
2023-12-17 18:33:58 +0900

    一旦、chat_room.rbのバリデーションを削除。機能する実装を考える。

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -3,11 +3,4 @@ class ChatRoom < ApplicationRecord
   has_many :chat_messages
   has_many :users, through: :chat_room_users
 
-  validate :users_count_within_limit, on: :create
-
-  private
-
-  def users_count_within_limit
-    errors.add(:base, 'チャットルームは既に2人が登録済みです。') if users.count >= 2
-  end
 end
