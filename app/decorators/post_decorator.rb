16606410da88f0964bd1d379be517391d77966cf
orikotanaka <oriko644@gmail.com>
2023-12-22 13:13:48 +0900

    レイアウト微修正。cssを適用することができた。

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -4,9 +4,9 @@ class PostDecorator < Draper::Decorator
   def formatted_likes(current_user)
     if likes.present?
       if current_user&.post_already_liked?(post.id)
-        h.content_tag(:i, '', class: 'fa-solid fa-heart ml-4 mr-2') + likes.count.to_s
+        h.content_tag(:i, '', class: 'fa-solid fa-heart mr-2') + likes.count.to_s
       else
-        h.content_tag(:i, '', class: 'fa-regular fa-heart ml-4 mr-2') + likes.count.to_s
+        h.content_tag(:i, '', class: 'fa-regular fa-heart mr-2') + likes.count.to_s
       end
     end
   end
