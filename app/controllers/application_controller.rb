7333fcb87c19366224f7742fc478630080904565
calmdown1987 <calmdown1987@icloud.com>
2024-01-05 22:11:26 +0900

    一般ユーザー側のリダイレクト先をrootからnew_user_session_pathに修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     case resource
     when :user
-      root_path
+      new_user_session_path
     when :admin
       new_admin_session_path
     when :manager
