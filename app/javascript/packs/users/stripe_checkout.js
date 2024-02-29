3b458c0d01719f74c35a6127282f2025dab4424e
calmdown1987 <calmdown1987@icloud.com>
2024-02-29 21:48:58 +0900

    jsでバックエンドへのリクエストを送信する処理を記載

---
+++ app/javascript/packs/users/stripe_checkout.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $('#goToOrderPage').click(function(){
+    confirmedOrder();
+  });
+});
+
+function confirmedOrder(){
+  var confirmOrderUrl = '/users/checkouts'; // 
+  $.ajax({
+    url: confirmOrderUrl,
+    type: 'POST',
+    success: function(data) {
+      window.location.href = data.session.url;
+    },
+    error: function(error) {
+      console.log(error);      
+    }
+  });
+}
