a1cc931760578527dd7b8fd2fbc9e923bef40a4e
calmdown1987 <calmdown1987@icloud.com>
2024-03-11 20:33:19 +0900

    jsによる合計金額のviewへの表示と無料選択時のフィールド制限機能を追加

--- app/javascript/packs/admins/charge.js
+++ app/javascript/packs/admins/charge.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
+
   var $priceField = $("#price_field");
   var $quantityField = $("#quantity_field");
+  var $chargeTypeSelect = $('#charge_plan_charge_type');
   var $totalAmount = $("#total_amount");
 
   function updateTotalAmount() {
@@ -10,8 +12,21 @@ $(document).ready(function() {
     $totalAmount.text("合計金額: " + total + "円");
   }
 
+  function handleChargeTypeChange() {
+
+    var isFree = $chargeTypeSelect.val() === '無料';
+    $priceField.val(isFree ? 0 : $priceField.data('default'));
+    $quantityField.val(isFree ? 0 : $quantityField.data('default'));
+    $priceField.prop('disabled', isFree);
+    $quantityField.prop('disabled', isFree);
+    $totalAmount.text("合計金額:" + (isFree ? "0" : total) + "円");
+  }
+
   $priceField.on("input", updateTotalAmount);
   $quantityField.on("input", updateTotalAmount);
+  $chargeTypeSelect.change(handleChargeTypeChange);
 
+  handleChargeTypeChange();
   updateTotalAmount();
+
 });
