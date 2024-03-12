6e2fc09cbf6dd52f064a8ca077bdc9512c219246
calmdown1987 <calmdown1987@icloud.com>
2024-03-12 13:34:50 +0900

    バリデーション機能を修正

--- app/models/charge_plan.rb
+++ app/models/charge_plan.rb
@@ -1,6 +1,6 @@
 class ChargePlan < ApplicationRecord
   validates :price, numericality: { greater_than: 0,}
   validates :quantity, numericality: { greater_than: 0,}
-  validates :amount, numericality: { greater_than: 0,}
+  validates :amount, presence: true
   validates :charge_type, presence: true
 end
