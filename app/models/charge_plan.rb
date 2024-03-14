3a8edfb4477934244f394b11a8379a45374d362f
calmdown1987 <calmdown1987@icloud.com>
2024-03-14 20:20:09 +0900

    決済機能に関する一覧画面を追加(修正)

--- app/models/charge_plan.rb
+++ app/models/charge_plan.rb
@@ -3,4 +3,11 @@ class ChargePlan < ApplicationRecord
   validates :quantity, numericality: { greater_than: 0,}
   validates :amount, presence: true
   validates :charge_type, presence: true
+
+  belongs_to :admin
+
+  def amount_calc(price, quantity)
+    price * quantity
+  end
+
 end
