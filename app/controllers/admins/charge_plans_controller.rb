ab010e8a5c0265002fe9141e67a938b82587f218
calmdown1987 <calmdown1987@icloud.com>
2024-03-19 14:20:46 +0900

    他のテナントの料金設定の閲覧を禁止する認可機能を追加

--- app/controllers/admins/charge_plans_controller.rb
+++ app/controllers/admins/charge_plans_controller.rb
@@ -1,55 +1,77 @@
 class Admins::ChargePlansController < Admins::Base
   before_action :check_double_charge, only: %i[new]
+  before_action :set_charge_plan, only: %i[confirm back create]
+  before_action :find_charge_plan, only: %i[show edit update]
+  before_action :check_charge_plan_owner, only: %i[show edit]
 
   def new
     @charge_plan = ChargePlan.new
   end
 
   def confirm
-    @charge_plan = ChargePlan.new(charge_plan_params)
     @charge_plan.admin_id = current_admin.id
     render :new if @charge_plan.invalid?
   end
 
   def back
-    @charge_plan = ChargePlan.new(charge_plan_params)
     @charge_plan.admin_id = current_admin.id
     render :new
   end
 
   def create
-    @charge_plan = ChargePlan.new(charge_plan_params)
     @charge_plan.admin_id = current_admin.id
-    @charge_plan.save
-    render :complete
+      if @charge_plan.save
+        render :complete
+      else
+        render :confrim
+      end
   end
 
   def complete
   end
 
   def show
-    @charge_plan = ChargePlan.find(params[:id])
   end
 
   def edit
-    @charge_plan = ChargePlan.find(params[:id])
   end
 
   def update
-    @charge_plan = ChargePlan.find(params[:id])
-    @charge_plan.update(charge_plan_params)
-    render action: :show
+    if @charge_plan.update(charge_plan_params)
+      render action: :show
+    else
+      render :edit
+    end
   end
 
   private
+
+    def set_charge_plan
+      @charge_plan = ChargePlan.new(charge_plan_params)
+    end
+
+    def find_charge_plan
+      @charge_plan = ChargePlan.find(params[:id])
+    end
 
     def charge_plan_params
       params.require(:charge_plan).permit(:admin_id, :price, :quantity, :amount, :charge_type)
     end
 
-    def check_double_charge
-      @charge_plan = ChargePlan.find_by(id: current_admin.charge_plan.id)
-      redirect_to admins_charge_plan_path(@charge_plan) if @charge_plan.present?
+    def check_double_charge   
+      charge_plan = current_admin.charge_plan
+      if @charge_plan.present?
+        redirect_to admins_charge_plan_path(@charge_plan)
+      end
     end
 
+    def check_charge_plan_owner
+        charge_plan = current_admin.charge_plan
+        if charge_plan.present?
+          @charge_plan = ChargePlan.find_by(id: current_admin.charge_plan.id)
+          if current_admin.id != @charge_plan.admin_id
+            redirect_to admins_dash_boards_path
+          end
+        end
+    end
 end
