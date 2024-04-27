e0ef41c871fbcde24dc7aa5b2a63d9299e113de5
calmdown1987 <calmdown1987@icloud.com>
2024-04-27 22:55:48 +0900

    herokuのエラー解消のためapplication.jsを追加

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
+// import 'bootstrap'
+// import '../javascript/application'
+import './count';
+import './count_t';
+import '../stylesheets/users/inquiries.scss';
+import '../stylesheets/users/posts.scss';
