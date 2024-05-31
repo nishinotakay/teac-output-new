2c678dbc3a26d6aeccc7570d2866f90c84614602
菊川 信一 <107759288+ShinichiKikukawa@users.noreply.github.com>
2024-05-31 21:17:12 +0900

    Delete app/models/chat_gpt.rb

--- app/models/chat_gpt.rb
+++
@@ -1,9 +0,0 @@
-class ChatGpt < ApplicationRecord
-  belongs_to :user
-
-  # 許可されるモードを追加
-  MODES = %w[teacher].freeze
-
-  validates :prompt, presence: true
-  validates :mode, inclusion: { in: MODES }
-end
