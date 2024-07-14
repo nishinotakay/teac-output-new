2c9a63314d53b203fee9ded624b5ba0a2d73a915
calmdown1987 <calmdown1987@icloud.com>
2024-07-14 16:08:42 +0900

    一般ユーザー側のフォルダ格納機能のViewを作成

--- app/javascript/packs/users/index_articles_and_dashboards.js
+++ app/javascript/packs/users/index_articles_and_dashboards.js
@@ -127,4 +127,9 @@ $(function(){
     })
     window.location.search = search
   })
+
+  $('#js-hamburger-menu, .folder-link').on('click', function () {
+    $('.folder-wrapper').slideToggle(500)
+    $('.hamburger-menu').toggleClass('hamburger-menu--open')
+  });
 });
