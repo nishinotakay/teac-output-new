46bf3c915b1a90af6f6835b9b70edf0c9b2173f8
TatsuyaUchida0326 <sopratico0326@gmail.com>
2024-09-26 12:42:04 +0900

    管理者にe-learning機能追加完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_25_082113) do
+ActiveRecord::Schema.define(version: 2024_09_26_025539) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -144,7 +144,7 @@ ActiveRecord::Schema.define(version: 2024_09_25_082113) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "completed", default: false
-    t.integer "learner_id", null: false
+    t.integer "learner_id"
     t.integer "learned_article_id", null: false
     t.integer "admin_id"
     t.index ["admin_id"], name: "index_learnings_on_admin_id"
