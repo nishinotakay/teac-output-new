26891e1c7d84a95522a9c62d9d9487bb4dc1a5e5
calmdown1987 <calmdown1987@icloud.com>
2024-10-14 11:14:21 +0900

    コメントアウトを削除、テスト内容をエラーメッセージの存在確認のみに修正

--- spec/requests/users/tweets_spec.rb
+++ spec/requests/users/tweets_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe "Users::Tweets", type: :request do
   describe "GET /index" do
     subject { get users_tweets_path }
 
-    # 正常系: 有効なアクセス
     context 'ログインユーザーが一覧画面にアクセスした場合' do
       before do
         sign_in user
@@ -25,7 +24,6 @@ RSpec.describe "Users::Tweets", type: :request do
       end
     end
 
-    # 異常系: 不正なアクセス
     context 'ログインしていないユーザーが一覧画面にアクセスした場合' do
       before do
         sign_out user
@@ -37,10 +35,10 @@ RSpec.describe "Users::Tweets", type: :request do
         expect(response).to redirect_to(new_user_session_path)
       end
 
-      it 'エラーメッセージが表示される「ログインもしくはアカウント登録してください。」' do
+      it 'エラーメッセージが存在する' do
         subject
         follow_redirect!
-        expect(response.body).to include("ログインもしくはアカウント登録してください。")
+        expect(flash[:alert]).to be_present
       end
     end
   end
@@ -48,7 +46,6 @@ RSpec.describe "Users::Tweets", type: :request do
   describe "GET /show" do
     subject { get users_tweet_path(tweet.id) }
 
-    # 正常系:有効なアクセス
     context 'ログインユーザーが詳細画面にアクセスした場合' do
       before do
         sign_in user
@@ -60,7 +57,6 @@ RSpec.describe "Users::Tweets", type: :request do
       end
     end
 
-    # 異常系:不正なアクセス
     context 'ログインしていないユーザーが詳細画面にアクセスした場合' do
       before do
         sign_out user
@@ -79,7 +75,6 @@ RSpec.describe "Users::Tweets", type: :request do
       sign_in user
     end
     
-    # 正常系: 有効なパラメーターで投稿
     context 'ログインユーザーが新規投稿をした場合' do
       subject { post users_tweets_path, params: valid_params, headers: { "HTTP_REFERER" => referrer_url } }
 
@@ -93,14 +88,13 @@ RSpec.describe "Users::Tweets", type: :request do
         expect { subject }.to change(Tweet, :count).by(1)
       end
 
-      it 'メッセージ「つぶやきを作成しました。」が表示される' do
+      it 'サクセスメッセージが存在する' do
         subject
         follow_redirect!
-        expect(response.body).to include("つぶやきを作成しました。")
+        expect(flash[:success]).to be_present
       end
     end
 
-    # 異常系: 無効なパラメーターで投稿
     context '空文字で新規投稿をした場合' do
       subject { post users_tweets_path, params: nil_params, headers: { "HTTP_REFERER" => referrer_url } }
 
@@ -110,14 +104,13 @@ RSpec.describe "Users::Tweets", type: :request do
         expect(response).to redirect_to(referrer_url)
       end
 
-      it 'エラーメッセージ「投稿内容を入力してください」が表示される' do
+      it 'エラーメッセージが存在する' do
         subject
         follow_redirect!
-        expect(response.body).to include("投稿内容を入力してください")
+        expect(flash[:error]).to be_present
       end
     end
 
-    # 異常系: 無効なパラメーターで投稿
     context '255文字以上の新規投稿をした場合' do
       subject { post users_tweets_path, params: invalid_params, headers: { "HTTP_REFERER" => referrer_url } }
 
@@ -127,17 +120,16 @@ RSpec.describe "Users::Tweets", type: :request do
         expect(response).to redirect_to(referrer_url)
       end
 
-      it 'エラーメッセージ「投稿内容は255文字以内で入力してください」が表示される。' do
+      it 'エラーメッセージが存在する' do
         subject
         follow_redirect!
-        expect(response.body).to include("投稿内容は255文字以内で入力してください")
+        expect(flash[:error]).to be_present
       end
     end
   end
 
   describe "GET / edit" do
 
-    # 正常系: 有効な操作
     context 'ログインユーザーが編集画面にアクセスした場合' do
       before do
         sign_in user
@@ -149,7 +141,6 @@ RSpec.describe "Users::Tweets", type: :request do
       end
     end
 
-    # 異常系: 不正な操作
     context 'ログインしていないユーザーが編集画面にアクセスした場合' do
       subject { get edit_users_tweet_path(tweet.id) }
 
@@ -163,14 +154,13 @@ RSpec.describe "Users::Tweets", type: :request do
         expect(response).to redirect_to(new_user_session_path)
       end
 
-      it 'エラーメッセージ「ログインもしくはアカウント登録してください。」が表示される' do
+      it 'エラーメッセージが存在する' do
         subject
         follow_redirect!
-        expect(response.body).to include("ログインもしくはアカウント登録してください。")
+        expect(flash[:alert]).to be_present
       end
     end
 
-    # 異常系: 不正な操作
     context 'ログインユーザーが他のユーザーの編集画面にアクセスした場合' do
       subject { get edit_users_tweet_path(another_user_tweet.id) }
 
@@ -184,10 +174,10 @@ RSpec.describe "Users::Tweets", type: :request do
         expect(response).to redirect_to(users_dash_boards_path)
       end
 
-      it 'エラーメッセージ「ログインもしくはアカウント登録してください。」が表示される' do
+      it 'エラーメッセージが存在する' do
         subject
         follow_redirect!
-        expect(response.body).to include("アクセスできません")
+        expect(flash[:alert]).to be_present
       end
     end
   end
@@ -197,55 +187,38 @@ RSpec.describe "Users::Tweets", type: :request do
       sign_in user
     end
 
-    # 正常系: 有効なパラメーターで更新
     context "ログインユーザーがつぶやきを更新した場合" do
-      it 'HTTPステータスコード302が表示される' do
+      it 'HTTPステータスコード302が返される' do
         patch users_tweet_path(tweet.id), params: valid_params
         expect(response).to have_http_status(302)
         expect(response).to redirect_to(users_tweets_url)
       end
 
-      it 'メッセージ「編集成功しました。」が表示される' do
+      it 'サクセスメッセージが存在する' do
         patch users_tweet_path(tweet.id), params: valid_params
         follow_redirect!
-        expect(response.body).to include("編集成功しました。")
+        expect(flash[:success]).to be_present
       end
 
-      it 'つぶやきが更新される(更新時に入力した文字列がデータベースに反映される)' do
+      it 'つぶやきが更新される' do
         new_post = "Update test tweet"
         patch users_tweet_path(tweet.id), params: { tweet: { post: new_post} }
         expect(tweet.reload.post).to eq(new_post)
       end
     end
 
-    # 異常系: 無効なパラメーターで更新
-    context "空文字でつぶやきを更新した場合" do
-      subject { patch users_tweet_path(tweet.id), params: nil_params, xhr: true }
-
+    context "空文字でつぶやきを更新した場合" do  
       it "HTTPステータスコード200が返される" do
-        subject
+        patch users_tweet_path(tweet.id), params: nil_params, xhr: true
         expect(response).to have_http_status(200)
-      end
-
-      it "エラーメッセージ「投稿内容を入力してください」が表示される" do
-        subject
-        expect(response.body).to include("投稿内容を入力してください")
       end
     end
 
-    # 異常系: 無効なパラメーターで更新
     context "255文字以上でつぶやきを更新した場合" do
-      subject { patch users_tweet_path(tweet.id), params: invalid_params, xhr: true }
-
       it "HTTPステータスコード200が返される" do
-        subject
+        patch users_tweet_path(tweet.id), params: invalid_params, xhr: true
         expect(response).to have_http_status(200)
-      end
-
-      it "エラーメッセージ「投稿内容は255文字以内で入力してください」が表示される" do
-        subject
-        expect(response.body).to include("投稿内容は255文字以内で入力してください")
-      end
+      end 
     end
   end
 
@@ -255,7 +228,6 @@ RSpec.describe "Users::Tweets", type: :request do
       @tweet = FactoryBot.create(:tweet, user: user)
     end
 
-    # 正常系: 有効な操作
     context 'ログインユーザーがつぶやきを削除した場合' do
       subject { delete users_tweet_path(@tweet.id) }
 
@@ -265,18 +237,17 @@ RSpec.describe "Users::Tweets", type: :request do
         expect(response).to redirect_to users_tweets_url
       end
 
-      it 'メッセージ「削除に成功しました。」が表示される' do
+      it 'サクセスメッセージが存在する' do
         subject
         follow_redirect!
-        expect(response.body).to include("削除に成功しました。")
+        expect(flash[:success]).to be_present
       end
 
-      it 'つぶやきが削除される(データベースからつぶやきが１つ減る)' do
+      it 'つぶやきが削除される' do
         expect { subject }.to change(Tweet, :count).by(-1)
       end
     end
 
-    # 異常系: 不正な操作
     context "ログインユーザーが別のユーザーの投稿を削除する操作をした場合" do
       subject { delete users_tweet_path(another_user_tweet.id) }
 
@@ -286,10 +257,10 @@ RSpec.describe "Users::Tweets", type: :request do
         expect(response).to redirect_to(users_dash_boards_path)
       end
 
-      it "エラーメッセージ「アクセスできません」が表示される" do
+      it "エラーメッセージが存在する" do
         subject
         follow_redirect!
-        expect(response.body).to include("アクセスできません")
+        expect(flash[:alert]).to be_present
       end
     end
   end
@@ -297,7 +268,6 @@ RSpec.describe "Users::Tweets", type: :request do
   describe "index_user" do
     subject { get index_user_users_tweet_path(user.id)}
 
-    # 正常系: 有効なアクセス
     context 'ログインユーザーが個別のユーザーの一覧画面にアクセスした場合' do
       before do
         sign_in user
@@ -309,7 +279,6 @@ RSpec.describe "Users::Tweets", type: :request do
       end
     end
 
-    # 異常系: 不正なアクセス
     context 'ログインしていないユーザーが個別のユーザーの一覧画面にアクセスした場合' do
       before do
         sign_out user
@@ -321,10 +290,10 @@ RSpec.describe "Users::Tweets", type: :request do
         expect(response).to redirect_to(new_user_session_path)
       end
 
-      it "エラーメッセージ「ログインもしくはアカウント登録してください。」が表示される" do
+      it "エラーメッセージが存在する" do
         subject
         follow_redirect!
-        expect(response.body).to include("ログインもしくはアカウント登録してください。")
+        expect(flash[:alert]).to be_present
       end
     end
   end
