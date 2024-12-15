e348a54c4c0bad0636d3b6238b40f8112f794b48
calmdown1987 <calmdown1987@icloud.com>
2024-12-15 19:16:16 +0900

    マルチテナント_管理者ユーザー登録機能の追加(リダイレクト先の修正)

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -12,9 +12,9 @@ module Admins
     # end
 
     # POST /resource
-    # def create
-    #   super
-    # end
+    def create
+       super
+    end
 
     # GET /resource/edit
     # def edit
@@ -58,8 +58,9 @@ module Admins
     # end
 
     # The path used after sign up for inactive accounts.
-    # def after_inactive_sign_up_path_for(resource)
-    #   super(resource)
-    # end
+    def after_inactive_sign_up_path_for(resource)
+       return super unless params[:tenant_id].present?
+       new_tenant_admin_admin_session_path(tenant_id: params[:tenant_id])
+     end
   end
 end
