8fdd7b79fdbb1351889b730b40fa93d694420297
calmdown1987 <calmdown1987@icloud.com>
2024-12-15 13:32:51 +0900

    マルチテナント_管理者のログイン・ログアウト機能の追加

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -11,14 +11,30 @@ module Admins
     # end
 
     # POST /resource/sign_in
-    # def create
-    #   super
-    # end
+    def create
+      return super unless params[:tenant_id].present? || session[:tenant_id].present?
+      tenant_id = params[:tenant_id] || session[:tenant_id]
+      tenant = Tenant.find_by(id: tenant_id)
+    
+      if tenant
+        session[:tenant_id] = tenant_id
+        super do |admin|
+          warden.set_user(admin, scope: :tenant_admin_admin)
+        end
+      else
+        flash[:alert] = "無効なテナントです"
+        redirect_to new_tenant_admin_session_path(tenant_id: tenant_id)
+      end
+    end
 
     # DELETE /resource/sign_out
-    # def destroy
-    #   super
-    # end
+    def destroy
+      return super unless session[:tenant_id].present?
+      warden.logout(:tenant_admin_admin)
+      warden.logout(:admin)
+      redirect_to new_tenant_admin_admin_session_path(tenant_id: session[:tenant_id])
+      session[:tenant_id] = nil
+    end
 
     # protected
 
