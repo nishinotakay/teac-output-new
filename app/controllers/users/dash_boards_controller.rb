a98d8f19cd63082b65bb78d6471d777c85adf8f1
calmdown1987 <calmdown1987@icloud.com>
2024-12-20 20:46:31 +0900

    管理者側_登録ユーザー一覧>詳細>投稿記事でエラーが発生する現象を解消

--- app/controllers/users/dash_boards_controller.rb
+++ app/controllers/users/dash_boards_controller.rb
@@ -16,25 +16,30 @@ module Users
       if current_admin.present? && current_user.nil?
         user = User.find(params[:user_id])
         @articles = user.articles.paginated_and_sort_filter(filter).page(params[:page]).per(30)
-      else
+      elsif current_user.present?
         @articles = current_user.articles.paginated_and_sort_filter(filter).page(params[:page]).per(30)
+      end
 
-      respond_to do |format|
+      if current_user.present?
+        respond_to do |format|
         format.any
         format.html
         format.json { render json: @articles }
-      end
+        end
       end
 
       if current_user.present?
         @folders = current_user.folders if current_user.folders.present?
       end
 
-      @folder_names = {}
-      @articles.each do |article|
-        latest_folder = ArticleFolder.where(article_id: article.id).order(created_at: :desc).first
-        @folder_names[article.id] = latest_folder.folder.name if latest_folder.present?
+      if current_user.present?
+        @folder_names = {}
+        @articles.each do |article|
+          latest_folder = ArticleFolder.where(article_id: article.id).order(created_at: :desc).first
+          @folder_names[article.id] = latest_folder.folder.name if latest_folder.present?
+        end
       end
     end
+
   end
 end
