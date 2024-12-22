288714ace62177d354159f48bd920b871f045211
calmdown1987 <calmdown1987@icloud.com>
2024-12-22 16:06:58 +0900

    tenant.rbのauthorized_tenant?のメソッド名をhas_user?に修正

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,7 +2,8 @@ class Tenant < ApplicationRecord
   has_many :users
   validates :name, presence: true, length: { in: 1..20 }
 
-  def authorized_tenant?(user)
+  def has_user?(user)
     user.tenant_id == self.id
   end
+
 end
