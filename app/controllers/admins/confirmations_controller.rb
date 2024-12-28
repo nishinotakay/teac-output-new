cf03038d07b39b65ba89cba35bde4ff87bd87afa
calmdown1987 <calmdown1987@icloud.com>
2024-12-28 17:21:05 +0900

    不要なadmins/confirmations_controller.rbファイルの取り下げ

--- app/controllers/admins/confirmations_controller.rb
+++ app/controllers/admins/confirmations_controller.rb
@@ -11,21 +11,23 @@ module Admins
     # POST /resource/confirmation
     # def create
     #   super
-    # endåç
+    # end
 
     # GET /resource/confirmation?confirmation_token=abcdef
-    # def create
+    # def show
+    #   super
+    # end
 
-    #protected
+    # protected
 
     # The path used after resending confirmation instructions.
     # def after_resending_confirmation_instructions_path_for(resource_name)
     #   super(resource_name)
     # end
 
-    #The path used after confirmation.
-    #def after_confirmation_path_for(resource_name, resource)
-      #new_tenant_admin_admin_session_path(tenant_id: resource.tenant_id)#   super(resource_name, resource)
-    #end
+    # The path used after confirmation.
+    # def after_confirmation_path_for(resource_name, resource)
+    #   super(resource_name, resource)
+    # end
   end
 end
