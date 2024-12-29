b54cf299897ffaf6d7146e8113c01aea64547c7d
calmdown1987 <calmdown1987@icloud.com>
2024-12-29 13:16:27 +0900

    シングルテナントとマルチテナントを統合したルーティングに修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
-  # 管理者関連
+  # admin関連=========================================================
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
@@ -41,20 +41,106 @@ Rails.application.routes.draw do
     resources :learnings, only: [:index, :show, :create]
   end
 
-  # ユーザー関連
+  # =================================================================
+
+  # user関連==========================================================
   devise_scope :user do
     root 'users/sessions#new'
   end
 
-  # シングルテナント用
-  devise_for :users, skip: [:omniauth_callbacks], controllers: {
+  devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     confirmations: 'users/confirmations',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  # マルチテナント用
+  namespace :users do
+    resources :dash_boards, only: [:index]
+    resources :chat_rooms, only: [:create, :show]
+    resources :stocks, only: [:create, :destroy, :index]
+    resources :learnings, only: [:index, :show, :create]
+    resources :folders, only: [:create, :show, :update, :destroy]
+    resources :checkouts, only: [:new, :create] do
+      get 'complete', on: :collection
+    end
+    resources :subscriptions, only: [:new, :create] do
+      get 'complete', on: :collection
+    end
+    resources :articles do
+      resources :article_comments, only: %i[create destroy update] # 記事コメント機能
+      resource :likes, only: [] do
+        post 'article_create', on: :member
+        delete 'article_destroy', on: :member
+      end
+      post 'assign_folder/:folder_id', to: 'article_folders#assign_folder', as: 'assign_folder'
+    end
+    resources :users do
+      resource :posts, only: [] do
+        get 'index_user', on: :member
+      end
+      resources :payments, only: [:index]
+      resource :relationships, only: [:index, :create, :destroy]
+        get :followings, :followers, on: :member
+    end
+    resources :users, only: [:show]
+    resources :posts do
+      resources :post_comments, only: %i[create destroy update]
+      resource :likes, only: [] do
+        post 'post_create', on: :member
+        delete 'post_destroy', on: :member
+      end
+    end
+    resources :articles
+    namespace :articles do
+      post 'image'
+    end
+    namespace :users do
+      resources :users, only: [:index]
+    end
+    resources :profiles
+    resources :tweets do # つぶやき機能
+      member do
+        get 'index_user'
+      end
+      resources :tweet_comments, only: %i[create destroy update] do # コメント機能
+        member do # 個々のコメントに対してアクセスできるカスタムアクションを定義する
+          patch 'confirmed_notification' # confirmed_notificationアクションに対するRESTfulなルーティングを定義
+        end
+      end
+    end
+    resources :inquiries
+  end
+  # =================================================================
+
+  # manager関連=======================================================
+  devise_for :managers, controllers: {
+    sessions:      'managers/sessions',
+    passwords:     'managers/passwords',
+    confirmations: 'users/confirmations',
+    registrations: 'managers/registrations'
+  }
+
+  namespace :managers do
+    resources :tenants, only: [:index, :show, :new, :create, :destroy]
+    resources :profiles do
+      collection do
+        get 'managers_show'
+      end
+    end
+  end
+  # =================================================================
+
+  # 共通==============================================================
+  # 利用規約
+  get 'use' => 'use#index'
+  # =================================================================
+  
+  # =================================================================
+
+  # マルチテナント=====================================================
+
   scope 'tenant/:tenant_id', as: 'tenant' do
     devise_for :users, skip: [:omniauth_callbacks], controllers: {
       sessions:      'users/sessions',
@@ -64,15 +150,15 @@ Rails.application.routes.draw do
     }, as: :tenant_user
   end
 
-  # OmniAuth コールバック
-  devise_for :users, only: :omniauth_callbacks, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  scope 'tenant/:tenant_id', as: 'tenant' do
+    devise_for :admins, controllers: {
+      sessions:      'admins/sessions',
+      passwords:     'admins/passwords',
+      confirmations: 'admins/confirmations',
+      registrations: 'admins/registrations'
+    }, as: :tenant_admin
+  end
 
-  # ユーザーのダッシュボード
-  get 'users/dash_boards', to: 'users/dash_boards#index', as: :users_dash_boards
-
-  # その他のユーザー関連ルート
   namespace :users do
     resources :chat_rooms, only: [:create, :show]
     resources :stocks, only: [:create, :destroy, :index]
@@ -90,14 +176,13 @@ Rails.application.routes.draw do
         post 'article_create', on: :member
         delete 'article_destroy', on: :member
       end
-      post 'assign_folder/:folder_id', to: 'article_folders#assign_folder', as: 'assign_folder'
     end
     resources :users do
       resource :posts, only: [] do
         get 'index_user', on: :member
       end
       resources :payments, only: [:index]
-      resource :relationships, only: [:index, :create, :destroy]
+      resources :relationships, only: [:index, :create, :destroy]
         get :followings, :followers, on: :member
     end
     resources :users, only: [:show]
@@ -124,24 +209,4 @@ Rails.application.routes.draw do
     end
     resources :inquiries
   end
-
-  # マネージャー関連
-  devise_for :managers, controllers: {
-    sessions:      'managers/sessions',
-    passwords:     'managers/passwords',
-    confirmations: 'users/confirmations',
-    registrations: 'managers/registrations'
-  }
-
-  namespace :managers do
-    resources :tenants, only: [:index, :show, :new, :create, :destroy]
-    resources :profiles do
-      collection do
-        get 'managers_show'
-      end
-    end
-  end
-
-  # 共通
-  get 'use', to: 'use#index'
 end
