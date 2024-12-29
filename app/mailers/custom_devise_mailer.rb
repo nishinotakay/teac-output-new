f42255ba55c64cd5a7f652116cdfdbc5d2281ad5
calmdown1987 <calmdown1987@icloud.com>
2024-12-29 11:47:29 +0900

    ユーザー作成時の認証メールのurl生成の不具合を修正

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -4,15 +4,16 @@ class CustomDeviseMailer < Devise::Mailer
 
   def confirmation_instructions(record, token, opts = {})
     if record.tenant_id.present?
-      opts[:template_path] = 'devise/mailer'
       if record.is_a?(Admin)
+        opts[:template_path] = 'admins/mailer'
         generated_url = tenant_admin_admin_confirmation_url(tenant_id: record.tenant_id, confirmation_token: token)
       else
+        opts[:template_path] = 'users/mailer'
         generated_url = tenant_user_user_confirmation_url(tenant_id: record.tenant_id, confirmation_token: token)
         Rails.logger.debug "Generated URL: #{generated_url}"
       end
     else
-      opts[:template_path] = 'users/mailer'
+      opts[:template_path] = 'devise/mailer'
       generated_url = user_confirmation_url(confirmation_token: token)
     end
 
