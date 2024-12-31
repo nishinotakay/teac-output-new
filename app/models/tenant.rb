bca7a75fef7bf05e4d30d335ffbc615a39ff16c6
calmdown1987 <calmdown1987@icloud.com>
2024-12-31 13:52:55 +0900

    同じ名前のテナント作成を防ぐバリデーションを追加

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,7 +1,7 @@
 class Tenant < ApplicationRecord
   has_many :users
   has_many :admins
-  validates :name, presence: true, length: { in: 1..20 }
+  validates :name, presence: true, uniqueness: true, length: { in: 1..20 }
 
   def has_user?(user)
     user.tenant_id == self.id
