62b8efff9e29f4dd862a8d778a58132229987b2d
calmdown1987 <calmdown1987@icloud.com>
2025-01-06 20:03:27 +0900

    シングルとマルチの一般ユーザーと管理者ユーザーのルーティングを統合

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,16 @@ Rails.application.routes.draw do
     registrations: 'admins/registrations'
   }
 
+  # マルチテナントのadmin
+  scope 'tenant/:tenant_id', as: 'tenant' do
+    devise_for :admins, controllers: {
+      sessions:      'admins/sessions',
+      passwords:     'admins/passwords',
+      confirmations: 'admins/confirmations',
+      registrations: 'admins/registrations'
+    }, as: :tenant_admin
+  end
+
   namespace :admins do
     resources :posts
     resources :dash_boards, only: [:index]
@@ -56,10 +66,20 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
+  # マルチテナントのuser
+  scope 'tenant/:tenant_id', as: 'tenant' do
+    devise_for :users, skip: [:omniauth_callbacks], controllers: {
+      sessions:      'users/sessions',
+      passwords:     'users/passwords',
+      confirmations: 'users/confirmations',
+      registrations: 'users/registrations'
+    }, as: :tenant_user
+  end
+
   namespace :users do
     resources :dash_boards, only: [:index]
     resources :chat_rooms, only: [:create, :show]
-    resources :stocks, only:[:create, :destroy, :index]
+    resources :stocks, only: [:create, :destroy, :index]
     resources :learnings, only: [:index, :show, :create]
     resources :folders, only: [:create, :show, :update, :destroy]
     resources :checkouts, only: [:new, :create] do
@@ -136,77 +156,4 @@ Rails.application.routes.draw do
   # 利用規約
   get 'use' => 'use#index'
   # =================================================================
-  
-  # =================================================================
-
-  # マルチテナント=====================================================
-
-  scope 'tenant/:tenant_id', as: 'tenant' do
-    devise_for :users, skip: [:omniauth_callbacks], controllers: {
-      sessions:      'users/sessions',
-      passwords:     'users/passwords',
-      confirmations: 'users/confirmations',
-      registrations: 'users/registrations'
-    }, as: :tenant_user
-  end
-
-  scope 'tenant/:tenant_id', as: 'tenant' do
-    devise_for :admins, controllers: {
-      sessions:      'admins/sessions',
-      passwords:     'admins/passwords',
-      confirmations: 'admins/confirmations',
-      registrations: 'admins/registrations'
-    }, as: :tenant_admin
-  end
-
-  namespace :users do
-    resources :chat_rooms, only: [:create, :show]
-    resources :stocks, only: [:create, :destroy, :index]
-    resources :learnings, only: [:index, :show, :create]
-    resources :folders, only: [:create, :show, :update, :destroy]
-    resources :checkouts, only: [:new, :create] do
-      get 'complete', on: :collection
-    end
-    resources :subscriptions, only: [:new, :create] do
-      get 'complete', on: :collection
-    end
-    resources :articles do
-      resources :article_comments, only: %i[create destroy update]
-      resource :likes, only: [] do
-        post 'article_create', on: :member
-        delete 'article_destroy', on: :member
-      end
-    end
-    resources :users do
-      resource :posts, only: [] do
-        get 'index_user', on: :member
-      end
-      resources :payments, only: [:index]
-      resources :relationships, only: [:index, :create, :destroy]
-        get :followings, :followers, on: :member
-    end
-    resources :users, only: [:show]
-    resources :posts do
-      resources :post_comments, only: %i[create destroy update]
-      resource :likes, only: [] do
-        post 'post_create', on: :member
-        delete 'post_destroy', on: :member
-      end
-    end
-    namespace :articles do
-      post 'image'
-    end
-    resources :profiles
-    resources :tweets do
-      member do
-        get 'index_user'
-      end
-      resources :tweet_comments, only: %i[create destroy update] do
-        member do
-          patch 'confirmed_notification'
-        end
-      end
-    end
-    resources :inquiries
-  end
 end
